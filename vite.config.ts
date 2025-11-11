14af9c009d37d0b1a4af368e9147a7170f85cb9b
自由人 <jiyuunin@jiyuunins-MacBook-Pro.local>
2025-11-11 14:26:13 +0000

    3rd update

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    sourcemap: false, // prevents exposing source maps
+  },
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
